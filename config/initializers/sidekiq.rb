6a7ba21e88c77c6ea5902ac3d2ebba87e7dcacfb
Ilija <ilija@webfactory.mk>
2016-10-19 15:00:57 +0200

    Setup Redis url to sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,3 @@
 Sidekiq.configure_server do |config|
-  config.redis = { driver: 'hiredis' }
+  config.redis = { driver: 'hiredis', url: 'redis://bikeindex:49dc2c3a3ee476d0f89706942be10d1e5ded6f7d6fe8214a2edae6ddd15e3a8f@dokku-redis-bikeindex:6379' }
 end
